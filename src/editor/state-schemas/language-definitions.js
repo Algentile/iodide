db54236edfa5f49aec5cc71cf7dcfac9b0ee8954
algentile <gogentileorgohome@gmail.com>
2019-09-29 22:34:35 -0700

    add language definitions

--- src/editor/state-schemas/language-definitions.js
+++ src/editor/state-schemas/language-definitions.js
@@ -23,6 +23,62 @@ const pyLanguageDefinition = {
   pluginType: "language"
 };
 
+const luaLanguageDefintion = {
+  languageId: "lua",
+  displayName: "Lua",
+  url: "https://cdn.jsdelivr.net/npm/fengari-web@0.1.4/dist/fengari-web.js",
+  module: "fengari",
+  evaluator: "evalLua",
+  pluginType: "language"
+};
+
+const rubyLanguageDefinition = {
+  languageId: "ruby",
+  displayName: "Ruby",
+  url: "https://cdn.opalrb.com/opal/current/opal.js",
+  module: "Opal",
+  evaluator: "eval",
+  pluginType: "language"
+};
+
+const ocamlLanguageDefinition = {
+  languageId: "ml",
+  displayName: "OCaml",
+  url: "https://louisabraham.github.io/domical/eval.js",
+  module: "evaluator",
+  evaluator: "execute",
+  pluginType: "language"
+};
+
+const juliaLanguageDefinition = {
+  languageId: "jl",
+  displayName: "Julia",
+  url: "https://keno.github.io/julia-wasm/julide/julide.js",
+  module: "jlodide",
+  evaluator: "runJulia",
+  pluginType: "language"
+};
+
+const plantumlLanguageDefinition = {
+  languageId: "pu",
+  displayName: "PlantUml",
+  url:
+    "https://raw.githubusercontent.com/six42/iodide-plantuml-plugin/master/src/iodide-plantuml-plugin.js",
+  module: "plantuml",
+  evaluator: "plantuml_img",
+  pluginType: "language"
+};
+
+const assemblyscriptLanguageDefinition = {
+  languageId: "as",
+  displayName: "AssemblyScript",
+  url:
+    "https://raw.githubusercontent.com/AssemblyScript/assemblyscript/master/dist/asc.js",
+  module: "asmScript",
+  evaluator: "eval",
+  pluginType: "language"
+};
+
 const fetchLanguageDefinition = {
   languageId: "fetch",
   displayName: "Fetch",
@@ -33,5 +89,11 @@ const fetchLanguageDefinition = {
 export const languageDefinitions = {
   py: pyLanguageDefinition,
   js: jsLanguageDefinition,
+  ocaml: ocamlLanguageDefinition,
+  julia: juliaLanguageDefinition,
+  as: assemblyscriptLanguageDefinition,
+  pu: plantumlLanguageDefinition,
+  lua: luaLanguageDefintion,
+  ruby: rubyLanguageDefinition,
   fetch: fetchLanguageDefinition
 };
